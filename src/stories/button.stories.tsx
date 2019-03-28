5e9d4bd97ac67b5ff70f8e22d0f438d67ae7fbe4
Ryan Lynch <lynch.r.j@gmail.com>
2019-03-28 11:49:05 -0400

    button stories

---
+++ src/stories/button.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import { Button } from '../components/button';
+
+const { add } = storiesOf('Button', module);
+
+add('colors!', () => (
+  <React.Fragment>
+    <Button label="Blue" color="blue" action={action('blue pressed')} />
+    <Button label="Puple" color="purple" action={action('purple pressed')} />
+    <Button label="Green" color="green" action={action('green pressed')} />
+  </React.Fragment>
+));
